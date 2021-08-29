08807d7c4f00223ed7b2cfd16651b95b41b40ee0
19521635 <19521635@gm.uit.edu.vn>
2021-08-30 00:14:03 +0700

    ✨ feat: add flash card component

---
+++ resources/js/components/SimpleFlashCard.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flashcard__body border border-2 border-dark text-dark p-3 fw-bold">Learn new vocabulary pronunc...</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped="scss">
+.flashcard__body {
+    width: 400px;
+    height: 200px;
+    border-radius: 1.5625rem;
+    background-color: #e6f4f4;
+}
+</style>
---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <simple-flash-card></simple-flash-card>
+    </div>
+</template>
+
+<script>
+import SimpleFlashCard from "../components/SimpleFlashCard.vue";
+export default {
+    name: "Home",
+    components: {
+        SimpleFlashCard,
+    },
+};
+</script>
+
+<style></style>
